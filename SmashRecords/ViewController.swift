615dded8e7c443ac5692b27c7506134071d56e2a
Yoshinobu <Yoshinobu>
2020-06-08 09:18:34 +0900

    initial commit

---
+++ SmashRecords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmashRecords
+//
+//  Created by 村尾慶伸 on 2020/06/08.
+//  Copyright © 2020 村尾慶伸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
